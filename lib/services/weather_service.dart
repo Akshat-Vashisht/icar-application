ef14433c430115a70a61507e468cbadd35b354de
AkshatVashisht <akashat2003@gmail.com>
2024-11-17 16:14:46 +0530

    fix(weather)!: add check to prevent unecessary API calls 1. created a new var _cachedWeather 2. add static to ensure only one instance of _cachedWeather variable 3. before the API call, if _cachedWeather has data, then return that data, else call the API
    Notes:
    TODO: Seems to have fixed the issue, need to do explicit research

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,18 +6,27 @@ import 'package:http/http.dart' as http;
 class WeatherService {
   static const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
+  static WeatherModel? _cachedWeather;
 
   WeatherService({required this.apiKey});
 
   Future<WeatherModel> getWeather(String cityName) async {
+    // Return cached weather data if available
+    if (_cachedWeather != null) {
+      // print('-------------------------------------USING CACHED DATA---------------------------------');
+      return _cachedWeather!;
+    }
+
     final url = '$baseURL?q=$cityName&appid=$apiKey&units=metric';
     final response = await http.get(Uri.parse(url));
-
+    // print("-------------------------------------CALLING API---------------------------------");
+    
     // // TODO: Remove this line after demonstration
     // await Future.delayed(Duration(seconds: 5));
 
     if (response.statusCode == 200) {
-      return WeatherModel.fromJson(jsonDecode(response.body));
+      _cachedWeather = WeatherModel.fromJson(jsonDecode(response.body));
+      return _cachedWeather!;
     } else {
       throw Exception('Failed to load weather data');
     }
